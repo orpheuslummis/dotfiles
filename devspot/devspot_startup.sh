014af74326077d34f4216046e50b50b7760bf8e8
Orpheus Lummis <o@orpheuslummis.info>
2021-10-24 04:40:57 +0000

    fix devspot startup

--- devspot/devspot_startup.sh
+++ devspot/devspot_startup.sh
@@ -8,7 +8,7 @@ sudo -u o sh -c '/home/o/dev/lib/openvscode-server-v1.61.0-linux-x64/server.sh &
 
 apt update
 apt -y install ncdu bat tmux git fzf
-apt -y curl gnupg software-properties-common curl build-essential
+apt -y install curl gnupg software-properties-common curl build-essential
 
 curl -fsSL https://deb.nodesource.com/setup_17.x | bash -
 apt-get install -y nodejs
@@ -16,4 +16,4 @@ apt-get install -y nodejs
 curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
 apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 apt-get update && apt-get install -y terraform
-terraform -install-autocomplete
+sudo -u o sh -c 'terraform -install-autocomplete'
